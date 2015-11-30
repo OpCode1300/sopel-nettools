4a3c0fb584feb2e090085f8df1f57b8fc98f9b84
Alex Newman <anewman@redhat.com>
2015-11-30 17:20:24 -0500

    first push

--- sopel_modules/nettools/nettools.py
+++ sopel_modules/nettools/nettools.py
@@ -1,18 +1,50 @@
 # coding=utf-8
+"""Network tools module"""
 
-from __future__ import unicode_literals, absolute_import, division, print_function
+from __future__ import unicode_literals, absolute_import, print_function, division
 
-from sopel import module
+import socket
+
+from sopel.module import commands, example
 
 
 def configure(config):
     pass
 
 
-def setup(bot):
+def setup(bot=None):
     pass
 
 
-@module.command('helloworld')
-def hello_world(bot, trigger):
-    bot.say('Hello, world!')
+def valid_ip(address):
+    try:
+        socket.inet_aton(address)
+        return True
+    except:
+        return False
+
+def lookupaddr(addr):
+    try:
+        return socket.gethostbyaddr(addr)
+    except socket.herror:
+        return None, None, None
+
+@commands('iplookup', 'ip')
+def ip(bot, trigger):
+    """IP Lookup tool"""
+    if not trigger.group(2):
+        return bot.reply("No search term.")
+    query = trigger.group(2)
+
+    if valid_ip(query):
+        # query is IP
+        hostname, aliaslist, ipaddrlist = lookupaddr(query)
+        if hostname is not None:
+            response = "Hostname: %s" % hostname
+        else:
+            response = "Unable to resolve hostname: %s" % query
+    else:
+        # query is hostname
+        ip = socket.gethostbyname(socket.getfqdn(query))
+        response = "IP: %s" % ip
+    bot.say(response)
